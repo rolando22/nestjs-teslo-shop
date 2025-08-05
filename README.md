02241a6ca89474f452b4731aa6e0c0647119c653
rolando22 <rolando2207@gmail.com>
2025-08-05 13:33:44 -0300

    add: connection to Postgres

--- README.md
+++ README.md
@@ -1,3 +1,24 @@
 <p align="center">
   <a href="http://nestjs.com/" target="blank"><img src="https://nestjs.com/img/logo-small.svg" width="120" alt="Nest Logo" /></a>
 </p>
+
+# Ejecutar en modo desarrollo
+
+1. Clonar el repositorio.
+2. Instalar dependencias.
+```
+npm install
+```
+3. Tener Nest CLI instalado.
+```
+npm i -g @nestjs/cli
+```
+4. Crear un archivo `.env` basado en `.env.template`.
+5. Levantar la base de datos y administrador gráfico con Docker.
+```
+docker compose up -d
+```
+6. Ejecutar la app.
+```
+npm run start:dev
+```
--- src/app.module.ts
+++ src/app.module.ts
@@ -1,6 +1,20 @@
 import { Module } from '@nestjs/common';
+import { ConfigModule } from '@nestjs/config';
+import { TypeOrmModule } from '@nestjs/typeorm';
 
 @Module({
-  imports: [],
+  imports: [
+    ConfigModule.forRoot(),
+    TypeOrmModule.forRoot({
+      type: 'postgres',
+      host: process.env.POSTGRES_HOST,
+      port: parseInt(process.env.POSTGRES_PORT || '5432', 10),
+      database: process.env.POSTGRES_DB,
+      username: process.env.POSTGRES_USER,
+      password: process.env.POSTGRES_PASSWORD,
+      autoLoadEntities: true,
+      synchronize: true,
+    }),
+  ],
 })
 export class AppModule {}
