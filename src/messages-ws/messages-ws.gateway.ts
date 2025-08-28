05a36ad039ef8fb2109c5c7184b280d721fe151b
rolando22 <rolando2207@gmail.com>
2025-08-28 17:48:57 -0300

    add: emit messages

--- src/messages-ws/messages-ws.gateway.ts
+++ src/messages-ws/messages-ws.gateway.ts
@@ -1,12 +1,14 @@
 import {
   OnGatewayConnection,
   OnGatewayDisconnect,
+  SubscribeMessage,
   WebSocketGateway,
   WebSocketServer,
 } from '@nestjs/websockets';
 import { Server, Socket } from 'socket.io';
 
 import { MessagesWsService } from './messages-ws.service';
+import { NewMessageDto } from './dtos/new-message.dto';
 
 @WebSocketGateway({ cors: true })
 export class MessagesWsGateway
@@ -21,9 +23,18 @@ export class MessagesWsGateway
     const connectedClients = this.messagesWsService.getConnectedClients();
     this.wss.emit('clients-updated', connectedClients);
   }
+
   handleDisconnect(client: Socket) {
     this.messagesWsService.removeClient(client.id);
     const connectedClients = this.messagesWsService.getConnectedClients();
     this.wss.emit('clients-updated', connectedClients);
+  }
+
+  @SubscribeMessage('message-from-client')
+  handleMessageFromClient(client: Socket, payload: NewMessageDto) {
+    this.wss.emit('message-from-server', {
+      fullName: client.id,
+      message: payload.message,
+    });
   }
 }
